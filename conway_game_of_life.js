ff65d4bec09becc931be579b8e4639d809f34e46
bukuta <c7h12bo@gmail.com>
2014-08-05 19:06:46 +0800

    conway_game of life

---
+++ conway_game_of_life.js
@@ -0,0 +1,13 @@
+/*
+ * 生成第N代
+ * 放大数组范围,可能开拓新的地盘
+ * 扫描原数组，每个位置的'气',判断在新位置的存活
+ * 裁剪新数组<=是否固定范围？
+ */
+function getGeneration(cells,generations,clip){
+  //input cells: array of arrays 
+
+  var rows=cells.length;
+  var cols=cells[0].length;
+
+}
